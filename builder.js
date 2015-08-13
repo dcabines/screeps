97b6906bdbd5564da79d3f79a8328dcc8103e05b
David Cabiness <dcabines@gmail.com>
2015-08-13 16:23:53 -0400

    builder is a bit smarter

--- builder.js
+++ builder.js
@@ -1,20 +1,43 @@
 module.exports = function (creep) {
-    if (creep.carry.energy === 0) {
-        creep.memory.action = 'refueling';
-    }
+	var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-	if (creep.carry.energy < creep.carryCapacity && creep.memory.action === 'refueling') {
-		creep.moveTo(Game.spawns.Spawn1);
-
-		var amount = Game.spawns.Spawn1.energy > creep.carryCapacity;
-		amount = amount ? creep.carryCapacity : Game.spawns.Spawn1.energy;
-
-        Game.spawns.Spawn1.transferEnergy(creep, amount);
+	if (creep.carry.energy === 0) {
+		creep.memory.action = 'refueling';
+	} else if (sites.length) {
+		creep.memory.action = 'building';
+	} else {
+		creep.memory.action = 'upgrading';
 	}
-	else {
-		var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
-		creep.moveTo(sites[0]);
-		creep.build(sites[0]);
-        creep.memory.action = 'building';
-	}
+
+	var actions = {
+		'refueling': function () {
+			creep.moveTo(Game.spawns.Spawn1);
+
+			var amount = Game.spawns.Spawn1.energy > creep.carryCapacity;
+			amount = amount ? creep.carryCapacity : Game.spawns.Spawn1.energy;
+
+			Game.spawns.Spawn1.transferEnergy(creep, amount);
+		},
+		'upgrading': function () {
+			var sites = creep.room.find(FIND_MY_STRUCTURES, {
+				filter: { structureType: STRUCTURE_RAMPART }
+			});
+
+			var site = _.min(sites, function (s) {
+				return s.hits;
+			});
+
+			creep.moveTo(site);
+			creep.repair(site);
+		},
+		'building': function () {
+			var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
+			
+			creep.moveTo(sites[0]);
+			creep.build(sites[0]);
+		}
+	};
+
+	actions[creep.memory.action]();
 }
+	
